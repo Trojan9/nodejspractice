d779a02cf2ac2dd0621a90b46e4d6708d564aa0a
unknown <Banjo Oluwatimilehin Emmanuel>
2021-12-22 18:02:29 +0100

    okay

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ try{
 }
    
 })
-const port = Process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000 ;
 app.listen(port,()=>{
     console.log("server listening to port "+port);
     
